0375b4791f92095152e9c17d16700dd85764fb6f
Sandro Lutz <sandro.lutz@temparus.ch>
2016-12-10 12:04:11 +0100

    Add test for creating a folder

--- lib/Hooks/UserHooks.php
+++ lib/Hooks/UserHooks.php
@@ -2,21 +2,25 @@
 namespace OCA\AmivCloudApp\Hooks;
 
 use OCA\AmivCloudApp\APIUtil;
+use OCP\Files\IRootFolder;
 
 class UserHooks {
 
     private $userManager;
     private $groupManager;
+    private $rootFolder;
     private $logger;
 
-    public function __construct($groupManager, $userManager, $logger) {
+    public function __construct($groupManager, $userManager, $rootFolder, $logger) {
         $this->userManager = $userManager;
         $this->groupManager = $groupManager;
+        $this->rootFolder = $rootFolder;
         $this->logger = $logger;
     }
 
     public function register() {
-        $this->userManager->listen('\OC\User', 'preLogin', array($this, 'preLogin'));
+        //$this->userManager->listen('\OC\User', 'preLogin', array($this, 'preLogin'));
+        $this->userManager->listen('\OC\User', 'postLogin', array($this, 'postLogin'));
     }
 
     public function preLogin($user, $password) {
@@ -37,14 +41,40 @@ class UserHooks {
                 $this->logger->info('User successfully created', array('app' => 'AmivCloudApp'));
             }
 
-            $this->groupManager->getUserGroups($nextCloudUser);
+            $nextCloudGroups = $this->groupManager->getUserGroups($nextCloudUser);
 
+            // TODO: update group assignments from AMIV API
+            // Attention! Some parts are pseude code!
+            //list($httpcode, $server_output) = APIUtil::post('???', '???');
+            // Add current assignments
+            /*foreach ($groups as $group) {
+                if ($group->hasShare && $this->groupManager->groupExists($group)) {
+                    $this->groupManager->get($group)->addUser($nextCloudUser);
+                }
+            }
 
+            // remove invalidated group assignments
+            foreach ($nextCloudGroups as $nextCloudGroup) {
+                $valid = false;
+                foreach ($groups as $group) {
+                    if ($nextCloudGroup->getGID() == $group) {
+                        $valid = true;
+                    }
+                }
+                if (!$valid) {
+                    $nextCloudGroup->removeUser($nextCloudUser);
+                }
+            }*/
 
         } else {
             if ($nextCloudUser == null || !$this->groupManager->isAdmin($user)) {
                 throw new \OC\User\LoginException();
             }
         }
+    }
+
+    public function postLogin(\OC\User\User $user) {
+        $this->rootFolder->getUserFolder('amivadmin')->newFolder('Test');
+        $this->logger->info('postLogin called', array('app' => 'AmivCloudApp'));
     }
 }
